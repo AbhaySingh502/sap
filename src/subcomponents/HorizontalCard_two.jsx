f82f91e04d724a8d8c80c66497d717d253a2858d
AbhaySingh502 <abhaysingh65534@gmail.com>
2024-04-11 19:58:43 +0530

    Chnged the horixontal card

--- src/subcomponents/HorizontalCard_two.jsx
+++ src/subcomponents/HorizontalCard_two.jsx
@@ -17,17 +17,9 @@ function HorizontalCard() {
             <div className="main w-full bg-slate-950 text-white flex justify-center items-center md:w-2/6"> {/* Added md:w-2/6 class */}
                 <div className="content pb-8 pt-8">
                     <div className="head text-5xl w-fit mb-8" style={{ borderBottom: '3px solid goldenrod' }}>
-                        <span>Black Bird</span>
+                        <span>GAS TURBINE ENGINE</span>
                     </div>
-                    <div className=" text-xl flex flex-col">
-                        <span className="mb-4">Catogery - Tier II, MALE</span>
-                        <span className="mb-4">
-                            Altitude - 5KM
-                        </span>
-                        <span className="mb-4">Maximum speed - 110 m/s</span>
-                        <span className="mb-4">Wingspan - 4.5 m</span>
-                        <span className="mb-4">Endurance - 10 hr</span>
-                    </div>
+
                 </div>
             </div>
         </Card>
